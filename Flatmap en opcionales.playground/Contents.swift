5e84d0731c592031d50d383a8cf902253393b95d
Swift Funcional <info@swiftfuncional.com>
2017-04-03 18:35:07 +0200

    Create extension: flatMap in optionals

--- Flatmap en opcionales.playground/Contents.swift
+++ Flatmap en opcionales.playground/Contents.swift
@@ -15,3 +15,14 @@ let user: User? = userData.flatMap {
 
 	return User(name: $0[0], password: $0[1], email: $0[2], age: age, country: $0[4])
 }
+
+extension Optional {
+
+	public func flatMap<T>(_ transform: @escaping (Wrapped) -> T?) -> T? {
+		guard let x = self else {
+			return nil
+		}
+
+		return transform(x)
+	}
+}
