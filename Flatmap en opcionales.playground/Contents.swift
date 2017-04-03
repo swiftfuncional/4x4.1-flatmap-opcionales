499e3ad0bd889d2853514085307d97a0eb668e6b
Swift Funcional <info@swiftfuncional.com>
2017-04-03 18:22:08 +0200

    Declare mutable user and construct user from userData array

--- Flatmap en opcionales.playground/Contents.swift
+++ Flatmap en opcionales.playground/Contents.swift
@@ -7,3 +7,9 @@ struct User {
 }
 
 let userData: [String]? = []
+
+var user: User?
+
+if let data = userData, data.count == 5, let age = Int(data[3]) {
+	user = User(name: data[0], password: data[1], email: data[2], age: age, country: data[4])
+}
