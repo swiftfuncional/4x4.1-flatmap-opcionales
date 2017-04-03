a8a36f0565ed0c3cb92428d63a648628592c7f16
Swift Funcional <info@swiftfuncional.com>
2017-04-03 18:33:01 +0200

    FlatMap checks if userData is not nil
    We can remove this check (let data = userData) and use syntactic sugar

--- Flatmap en opcionales.playground/Contents.swift
+++ Flatmap en opcionales.playground/Contents.swift
@@ -9,9 +9,9 @@ struct User {
 let userData: [String]? = []
 
 let user: User? = userData.flatMap {
-	guard let data = userData, data.count == 5, let age = Int(data[3]) else {
+	guard $0.count == 5, let age = Int($0[3]) else {
 		return nil
 	}
 
-	return User(name: data[0], password: data[1], email: data[2], age: age, country: data[4])
+	return User(name: $0[0], password: $0[1], email: $0[2], age: age, country: $0[4])
 }
